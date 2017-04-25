c1a63139752fffd212ca376d2839badc3f403aca
bernard.wittwer <bernard.wittwer@leshop.ch>
2017-04-25 11:00:09 +0200

    #13 - stucks if the initialMax is a small numbe

---
+++ src/test/java/me/tongfei/progressbar/Issue13Test.java
@@ -0,0 +1,46 @@
+/*
+ * (c) LeShop SA 2017
+ */
+package me.tongfei.progressbar;
+
+import org.junit.Test;
+
+/**
+ * @author bwittwer
+ */
+public class Issue13Test {
+
+	private static final int NBR_ELEMENTS = 100;
+	private static final int PROGRESSBAR_GRACE_PERIOD = 1000;
+
+	@Test
+	public void testOk() {
+		ProgressBar pb = new ProgressBar("Test", NBR_ELEMENTS);
+		pb.start();
+
+		try {
+			Thread.sleep(PROGRESSBAR_GRACE_PERIOD);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+
+		for (int i = 0; i < 100; i++) {
+			pb.step();
+		}
+
+		pb.stop();
+	}
+
+	@Test
+	public void testKo() {
+		ProgressBar pb = new ProgressBar("Test", NBR_ELEMENTS);
+		pb.start();
+
+		for (int i = 0; i < 100; i++) {
+			pb.step();
+		}
+
+		pb.stop();
+	}
+
+}
